b46de9f6431038dc8ca49e2020d5431a573411b3
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-13 13:56:26 -0500

    Display Initals Program

---
+++ src/outputExamples/DisplayInitals.java
@@ -0,0 +1,9 @@
+package outputExamples;
+
+public class DisplayInitals
+{
+	public static void main(String[] args)
+	{
+		System.out.println("    JJJ     JJJ RRRRRR\n    JJJ     JJJ RR   RR\n    JJJ     JJJ RRRRRR\nJJ  JJJ JJ  JJJ RR  RR\n JJJJJ   JJJJJ  RR   RR");
+	}
+}
