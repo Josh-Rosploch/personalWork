a59e6dd6daf1fa61e8d0cadbe689ea0c66b21250
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-13 13:47:43 -0500

    Like and Dislike with Schedule Reformat

---
+++ src/outputExamples/DislikeLike.java
@@ -0,0 +1,9 @@
+package outputExamples;
+
+public class DislikeLike
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Like\t\tDislike\n-------\t\t-------\nCats\t\tDogs\nTrees\t\tThorns\nComputers\tMacs\nTomatos\t\tAvacado\nPizza\t\tPotatos");
+	}
+}
--- src/outputExamples/DisplaySchedule.java
+++ src/outputExamples/DisplaySchedule.java
@@ -4,10 +4,6 @@ public class DisplaySchedule
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Josh Rosploch");
-		System.out.println("Sports Officiating");
-		System.out.println("Digital Imaging and Design");
-		System.out.println("Algebra II");
-		System.out.println("AP Computer Science A");
+		System.out.println("Josh Rosploch\nSports Officiating\nDigital Imaging and Design\nAlgebra II\nAP Computer Science A");
 	}
 }
