71aa6aae90444ea696eab2440775eaa68fcb732e
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-13 13:51:50 -0500

    Display Address Program

---
+++ src/outputExamples/DisplayAddress.java
@@ -0,0 +1,9 @@
+package outputExamples;
+
+public class DisplayAddress
+{
+	public static void main(String[] args)
+	{
+		System.out.println("JOSH ROSPLOCH\n575 FALCON DRIVE\nBROOKFIELD, WI 53005");
+	}
+}
