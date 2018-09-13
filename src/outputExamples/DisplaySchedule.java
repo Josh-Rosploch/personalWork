2de5b81deed318880e471b5b7399ac9878f92b15
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-13 13:40:39 -0500

    Display Schedule Program

---
+++ src/outputExamples/DisplaySchedule.java
@@ -0,0 +1,13 @@
+package outputExamples;
+
+public class DisplaySchedule
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Josh Rosploch");
+		System.out.println("Sports Officiating");
+		System.out.println("Digital Imaging and Design");
+		System.out.println("Algebra II");
+		System.out.println("AP Computer Science A");
+	}
+}
