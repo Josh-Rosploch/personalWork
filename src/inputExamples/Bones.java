95c986d5ca46104c1b3742a9846c63eb183dc7cc
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-20 13:13:21 -0500

    Bones program

---
+++ src/inputExamples/Bones.java
@@ -0,0 +1,13 @@
+package inputExamples;
+
+import java.util.Scanner;
+
+public class Bones {
+	public static void main(String[] args) {
+		int bones = -999;
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many bones have you broke in your life?");
+		bones = input.nextInt();
+		System.out.println("You broke " + bones + " bones in your life.");
+	}
+}
--- src/inputExamples/Vote.java
+++ src/inputExamples/Vote.java
@@ -6,8 +6,8 @@ public class Vote {
 	public static void main(String[] args) {
 		boolean vote = false;
 		Scanner input = new Scanner(System.in);
-		System.out.println("True or False: Can you vote?");
+		System.out.println("True or False: Did you vote?");
 		vote = input.nextBoolean();
-		System.out.println("It is " + vote + " that you can vote.");
+		System.out.println("It is " + vote + " that you voted.");
 	}
 }
