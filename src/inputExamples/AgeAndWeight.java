0e10f0cdc66c53ea307ef8d75a405d7385ba7779
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-20 13:47:42 -0500

    Fortune Teller program

--- src/inputExamples/AgeAndWeight.java
+++ src/inputExamples/AgeAndWeight.java
@@ -7,12 +7,11 @@ public class AgeAndWeight {
 		int age = -999;
 		double weight = -999.0;
 		Scanner input = new Scanner(System.in);
-		System.out.println("What is your age?");
+		System.out.println("-------------------------\nWhat is your age?");
 		age = input.nextInt();
-		System.out.println("Your age is " + age);
-		System.out.println("What is your weight in pounds?");
+		System.out.println("Your age is " + age + ".\n-------------------------\nWhat is your weight in pounds?");
 		weight = input.nextDouble();
-		System.out.println("Your age is " + weight + " pounds.");
+		System.out.println("Your weight is " + weight + " pounds.\n-------------------------");
 		input.close();
 	}
 }
