dac6c52aa7c6f745de0b5295be109e1ef8dc9ba6
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-21 13:53:19 -0500

    Combo program edit

--- src/inputExamples/Combo.java
+++ src/inputExamples/Combo.java
@@ -8,13 +8,13 @@ public class Combo {
 		int combo2 = -99;
 		int combo3 = -99;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the first number in your lock combo:");
+		System.out.println("-------------------------\nEnter the first number in your lock combo:");
 		combo1 = input.nextInt();
-		System.out.println("Enter the second number in your lock combo:");
+		System.out.println("-------------------------\nEnter the second number in your lock combo:");
 		combo2 = input.nextInt();
-		System.out.println("Enter the third number in your lock combo:");
+		System.out.println("-------------------------\nEnter the third number in your lock combo:");
 		combo3 = input.nextInt();
 		input.close();
-		System.out.println("Your combo is: " + combo1 + " - " + combo2 + " - " + combo3);
+		System.out.println("-------------------------\nYour combo is: " + combo1 + " - " + combo2 + " - " + combo3 + "\n-------------------------");
 	}
 }
