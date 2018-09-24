1b99b7fe66d4bb74c43361a5d54dbdcf9629276d
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-24 14:16:30 -0500

    Math.PI

--- src/mathOpp/AreaOfCircle.java
+++ src/mathOpp/AreaOfCircle.java
@@ -9,7 +9,7 @@ public class AreaOfCircle {
 		Scanner input = new Scanner(System.in);
 		System.out.println("What is the radius of the circle?");
 		r = input.nextDouble();
-		r = 3.14159265359 * r * r;
+		r = Math.PI * r * r;
 		System.out.println("The area of the circle is: " + r);
 		input.close();
 	}
