aaabb909637b9f9a70e44291ff2036b4b23a237e
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-25 15:00:27 -0500

    Paint Change and Grass

---
+++ src/mathOpp/MoreCuttinGrass.java
@@ -0,0 +1,12 @@
+package mathOpp;
+
+import java.util.Scanner;
+
+public class MoreCuttinGrass {
+
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			double  = -999.0;
+			Scanner input = new Scanner(System.in);
+			System.out.println("");
+			
---
+++ src/mathOpp/PullPhoneNumberApart.java
@@ -0,0 +1,10 @@
+package mathOpp;
+
+import java.util.Scanner;
+
+public class PullPhoneNumberApart {
+	public static void main(String[] args) {
+		double  = -999.0;
+		Scanner input = new Scanner(System.in);
+		System.out.println("");
+		
