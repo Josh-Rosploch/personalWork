b591f51fe7ba7caf69f7a088b74194943f668266
Josh Rosploch <josh.rosploch@gmail.com>
2018-09-27 13:43:09 -0500

    Phone Number

--- src/mathOpp/PullPhoneNumberApart.java
+++ src/mathOpp/PullPhoneNumberApart.java
@@ -4,7 +4,19 @@ import java.util.Scanner;
 
 public class PullPhoneNumberApart {
 	public static void main(String[] args) {
-		double  = -999.0;
+		int phonenum = -999;
+		int first = -999;
+		int second = -999;
+		int third = -999;
 		Scanner input = new Scanner(System.in);
-		System.out.println("");
-		
+		System.out.println("What is your phone number?");
+		phonenum = input.nextInt();
+		first = (phonenum / 10000000);
+		second = (phonenum / 10000) % 1000;
+		third = (phonenum % 10000);
+		input.close();
+		System.out.println("Your phone number is: (" + first + ")" + second + "-" + third + "\nThe area code is: " + first
+				+ "\nThe middle three digits are: " + second + "\nThe last four digits are: "
+				+ third);
+	}
+}
