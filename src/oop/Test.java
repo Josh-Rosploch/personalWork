51c74966b20b1e8d01039be540e570303b27859a
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-01 14:48:38 -0500

    More thingys xD

--- src/oop/Test.java
+++ src/oop/Test.java
@@ -3,12 +3,11 @@ package oop;
 public class Test {
 
 	public static void main(String[] args) {
-		Greeter bob = new Greeter();
-		Friend joe = new Friend();
-		bob.sayHello();
-		bob.sayHello();
-		bob.sayHello();
-		joe.sayMessage();
-		bob.sayHello();
+		Friend jenny = new Friend();
+		Friend billy = new Friend();
+		Friend sammy = new Friend();
+		Friend kyle = new Friend();
+		billy.sayMessage();
+		kyle.sayMessage();
 	}
 }
