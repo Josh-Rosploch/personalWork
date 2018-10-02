cc781d0044fb896c99b29effbc7ecf4b8b265509
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-02 14:04:58 -0500

    kid

---
+++ src/oop/Kid.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Kid {
+	public Kid() {
+	}
+
+	public void sayMama() {
+		System.out.println("MAMA!");
+	}
+
+	public void sayFood() {
+		System.out.println("FOOD!");
+	}
+
+	public void sayStink() {
+		System.out.println("STINK!");
+	}
+}
---
+++ src/oop/KidTest.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class KidTest {
+	public static void main(String[] args) {
+		Kid jearmy = new Kid();
+		jearmy.sayMama();
+		jearmy.sayFood();
+		jearmy.sayStink();
+	}
+}
