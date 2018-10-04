3b70ffd17313f50234b8125350315e8c64103730
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-04 13:58:01 -0500

    Numbers

---
+++ src/oop/Numbers.java
@@ -0,0 +1,18 @@
+package oop;
+
+public class Numbers {
+	public Numbers() {
+	}
+
+	public void sayNumber(double num) {
+		System.out.println("double: " + num);
+	}
+
+	public void sayNumberPlus2(double num) {
+		System.out.println("double: " + (num + 2));
+	}
+
+	public void saySum(double num1, double num2) {
+		System.out.println("double: " + (num1 + num2));
+	}
+}
---
+++ src/oop/NumbersTest.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class NumbersTest {
+
+	public static void main(String[] args) {
+		Numbers bob = new Numbers();
+		bob.sayNumber(7.5);
+		bob.sayNumberPlus2(7.5);
+		bob.saySum(10.7, 5.3);
+	}
+}
