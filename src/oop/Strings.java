15f56afca1c5937c6fb75c0402a127826b824010
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-04 13:32:34 -0500

    Math Strings test

---
+++ src/oop/Strings.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		double num1 = 7.1;
+		double num2 = 9.6;
+		System.out.println(Math.pow(num1, 3));
+		System.out.println(Math.floor(num2));
+		System.out.println(Math.ceil(num1));
+	}
+}
