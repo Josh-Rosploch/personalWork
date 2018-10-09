39da97273822ea2a98fc211f730c061cb38f79ea
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-09 14:59:50 -0500

    Employyereee

--- src/oop/Employee.java
+++ src/oop/Employee.java
@@ -1,17 +1,31 @@
 package oop;
 
 public class Employee {
-	private int wage;
+	private double wage;
+	private int experience;
 
 	public Employee() {
-		wage = -999;
+		wage = -999.0;
+		experience = -999;
 	}
 
-	public int getWage() {
+	public double getWage() {
 		return wage;
 	}
 
-	public void setWage(int newWage) {
+	public void setWage(double newWage) {
 		wage = newWage;
+	}
+
+	public int getExperience() {
+		return experience;
+	}
+
+	public void setExperience(int newExperience) {
+		experience = newExperience;
+	}
+
+	public void giveRaise() {
+		wage = wage * .1 + wage;
 	}
 }
