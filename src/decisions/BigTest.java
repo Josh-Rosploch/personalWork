26c6a63b32e42b763ec9c803e382d881cb4fbdf3
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-17 14:52:02 -0500

    betterbignum

--- src/decisions/BigTest.java
+++ src/decisions/BigTest.java
@@ -3,22 +3,19 @@ package decisions;
 import java.util.Scanner;
 
 public class BigTest {
-
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter three numbers:");
 		double numOne = input.nextDouble();
 		double numTwo = input.nextDouble();
 		double numThree = input.nextDouble();
-		if (numOne > numTwo && numOne > numThree) {
-			System.out.println(numOne);
+		if (numOne < numTwo) {
+			numOne = numTwo;
 		}
-		if (numTwo > numOne && numTwo > numThree) {
-			System.out.println(numTwo);
+		if (numOne < numThree) {
+			numOne = numThree;
 		}
-		if (numThree > numTwo && numThree > numOne) {
-			System.out.println(numThree);
-		}
+		System.out.println("----------\n" + numOne);
 		input.close();
 	}
 }
