51a93ae7ddf52bd4d4f17e47ae7d7eee00f6f9ab
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-24 14:55:28 -0500

    quyick fix

--- src/decisions/DoWhile.java
+++ src/decisions/DoWhile.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class DoWhile {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		double number = -9999;
 		System.out.println("Please enter a number :");
-		number = input.nextDouble();
+		double number = input.nextDouble();
 		do {
 			System.out.println("Magic product is : " + (number * 17));
 			System.out.println("Please enter a number (9999 to quit) :");
