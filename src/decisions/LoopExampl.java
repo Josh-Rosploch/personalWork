71a91054d3041fc134e4ef95b82807b24ccd9a55
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-29 13:40:59 -0500

    idk

--- src/decisions/LoopExampl.java
+++ src/decisions/LoopExampl.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class LoopExampl {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int k = input.nextInt();
-		for (int j = 1; j <= 10; j++) {
-			System.out.println(k + " Hello");
+		int k = 0;
+		for (int j = 1; j <= 5; j++) {
+			System.out.println("Enter a number :");
+			k += input.nextInt();
 		}
+		System.out.println(k + " Hello");
 		input.close();
 	}
 }
