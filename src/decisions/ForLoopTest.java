327957fb9d1ccc42c1e73df9875c447597fd0e93
Josh Rosploch <josh.rosploch@gmail.com>
2018-10-30 14:56:24 -0500

    LOOP test

---
+++ src/decisions/ForLoopTest.java
@@ -0,0 +1,10 @@
+package decisions;
+
+public class ForLoopTest {
+	public static void main(String[] args) {
+		for (int n = 5; n != 5;) {
+			System.out.println("How Many?");
+			n = 5;
+		}
+	}
+}
