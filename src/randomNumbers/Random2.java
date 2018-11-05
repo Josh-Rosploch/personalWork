fe7d00a26446e9cd75cfe8504f842db2292ad1fe
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-05 14:16:55 -0600

    move

--- src/randomNumbers/Random2.java
+++ src/randomNumbers/Random2.java
@@ -1,4 +1,4 @@
-package decisions;
+package randomNumbers;
 
 import java.util.Random;
 
--- src/randomNumbers/random.java
+++ src/randomNumbers/random.java
@@ -1,4 +1,4 @@
-package decisions;
+package randomNumbers;
 
 public class random {
 	public static void main(String[] args) {
