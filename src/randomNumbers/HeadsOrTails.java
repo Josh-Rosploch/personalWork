de2c88e09cdb83cebce3c8aedef138a2f09efe7c
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-05 14:23:48 -0600

    coin flip

--- src/randomNumbers/HeadsOrTails.java
+++ src/randomNumbers/HeadsOrTails.java
@@ -5,9 +5,17 @@ import java.util.Random;
 public class HeadsOrTails {
 	public static void main(String[] args) {
 		Random rng = new Random();
-		int min = -50;
-		int max = 50;
-		int rndNum = min + rng.nextInt(max - min + 1);
-		System.out.println(rndNum);
+		int min = 1;
+		int max = 2;
+		int heads = 0;
+		int tails = 0;
+		for (int flips = 1; flips <= 100; flips++) {
+			if (min == (min + rng.nextInt(max - min + 1))) {
+				heads++;
+			} else {
+				tails++;
+			}
+		}
+		System.out.println("Total Heads: " + heads + "\nTotal Tails: " + tails);
 	}
 }
