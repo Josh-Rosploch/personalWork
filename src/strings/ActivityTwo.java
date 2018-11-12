a0ff595bd8bb4995934ece573eefb4feec691f2d
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-12 14:50:55 -0600

    activity two

--- src/strings/ActivityTwo.java
+++ src/strings/ActivityTwo.java
@@ -2,13 +2,12 @@ package strings;
 
 import java.util.Scanner;
 
-public class ActivityOne {
+public class ActivityTwo {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a sentence:");
 		String sentence = input.nextLine();
-		System.out.println("The first letter is: " + sentence.charAt(0));
-		System.out.println("The third letter is: " + sentence.charAt(2));
+		System.out.println("It is " + sentence.contains("Bob") + " that 'Bob' is in the sentence.");
 		System.out.println("The sentence is: " + sentence);
 		input.close();
 	}
