b5fe0d8ec9c664f507a64bce67d427dff196c472
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-12 13:55:49 -0600

    Yahzeev2

--- src/randomNumbers/Yahtzeev2.java
+++ src/randomNumbers/Yahtzeev2.java
@@ -19,7 +19,7 @@ public class Yahtzeev2 {
 			d5 = (1 + generator.nextInt(6));
 			rolls++;
 			System.out.println(d1 + " | " + d2 + " | " + d3 + " | " + d4 + " | " + d5);
-		} while ();
+		} while (d1 != d2 && d2 != d3 && d3 != d4 && d4 != d5);
 		System.out.println("You rolled five " + d1 + "'s after " + rolls + " rolls for Yahtzee");
 	}
 }
