bed1e025d9207294f5d14b93c2c5d219368c79d5
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-26 14:44:57 -0600

    TEST SCORES REVERSE

--- src/arrays/testScores.java
+++ src/arrays/testScores.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class testScores {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter FIVE test scores.");
-		double[] scores = new double[5];
-		int totalScore = 0;
-		for (int j = 0; j < 5; j++) {
+		int length = 5;
+		double[] scores = new double[length];
+		System.out.println("Enter " + length + " test scores.");
+		for (int j = 0; j < length; j++) {
 			scores[j] = input.nextDouble();
 		}
-		for (int j = 0; j < 5; j++) {
-			totalScore += scores[j];
+		for (int j = (length - 1); j >= 0; j--) {
+			System.out.println("Score " + (j + 1) + " is: " + scores[j]);
 		}
-		System.out.println("The sum of the scores is: " + totalScore);
 		input.close();
 	}
 }
