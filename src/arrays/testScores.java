598ec7ec5db70eafee67422dc096a9f3b76bf3ff
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-26 14:30:39 -0600

    Test scores do ttaol

--- src/arrays/testScores.java
+++ src/arrays/testScores.java
@@ -7,12 +7,14 @@ public class testScores {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter FIVE test scores.");
 		double[] scores = new double[5];
+		int totalScore = 0;
 		for (int j = 0; j < 5; j++) {
 			scores[j] = input.nextDouble();
 		}
 		for (int j = 0; j < 5; j++) {
-			System.out.println("Score " + (j + 1) + " is: " + scores[j]);
+			totalScore += scores[j];
 		}
+		System.out.println("The sum of the scores is: " + totalScore);
 		input.close();
 	}
 }
