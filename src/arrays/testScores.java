696dba76e4f9d30da28a219723317d1f4d3c39aa
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-27 13:59:41 -0600

    dotlength

--- src/arrays/testScores.java
+++ src/arrays/testScores.java
@@ -5,13 +5,12 @@ import java.util.Random;
 public class testScores {
 	public static void main(String[] args) {
 		Random rng = new Random(5985);
-		int length = 250;
 		double largest = 0;
-		double[] scores = new double[length];
-		for (int j = 0; j < length; j++) {
+		double[] scores = new double[250];
+		for (int j = 0; j < scores.length; j++) {
 			scores[j] = rng.nextInt(1001);
 		}
-		for (int j = 0; j < length; j++) {
+		for (int j = 0; j < scores.length; j++) {
 			if (largest < scores[j]) {
 				largest = scores[j];
 			}
