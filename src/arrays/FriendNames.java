e989bacddb1fc40a2e67736f89121adc4e46aa95
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-27 14:18:52 -0600

    friend names

---
+++ src/arrays/FriendNames.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class FriendNames {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String[] names = new String[5];
+		for (int n = 0; n < names.length; n++) {
+			System.out.println("Enter the name of friend " + (n + 1) + ".");
+			names[n] = input.nextLine();
+		}
+		for (int n = (names.length - 1); n >= 0; n--) {
+			System.out.println("Friend " + (n + 1) + "'s name is: " + names[n]);
+		}
+	}
+}
