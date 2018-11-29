6f7739e0e1fabe59fde56d990ebc67a1c07f54e3
Josh Rosploch <josh.rosploch@gmail.com>
2018-11-29 12:50:39 -0600

    Im sick?

--- src/arrays/RectangleTest.java
+++ src/arrays/RectangleTest.java
@@ -8,15 +8,27 @@ public class RectangleTest {
 		Scanner input = new Scanner(System.in);
 		Random rng = new Random();
 		final int numRectangles = 100;
-		RectangleClass[] holes = new RectangleClass[numRectangles];
-		for (int j = 0; j < holes.length; j++) {
-			holes[j] = new RectangleClass((10 + rng.nextInt(56 - 10 + 1)), (10 + rng.nextInt(56 - 10 + 1)));
+		RectangleClass[] rectang = new RectangleClass[numRectangles];
+		for (int j = 0; j < rectang.length; j++) {
+			rectang[j] = new RectangleClass((10 + rng.nextInt(56 - 10 + 1)), (10 + rng.nextInt(56 - 10 + 1)));
 		}
-		for (int j = 0; j < holes.length; j++) {
-			System.out.println("Length of rectangle " + (j + 1) + ":  " + holes[j].getLength());
-			System.out.println("Width of rectangle " + (j + 1) + ":  " + holes[j].getWidth());
-			System.out.println("Area of rectangle " + (j + 1) + ":  " + holes[j].area());
+		int bigArea = rectang[0].area();
+		int bigIndex = 0;
+		for (int j = 0; j < rectang.length; j++) {
+			if (rectang[j].area() > bigArea) {
+				bigArea = rectang[j].area();
+				bigIndex = j;
+			}
+		}
+		RectangleClass zero = new RectangleClass(rectang[0].getLength(), rectang[0].getWidth());
+		rectang[0] = rectang[bigIndex];
+		rectang[bigIndex] = zero;
+		for (int j = 0; j < rectang.length; j++) {
+			System.out.println("Length of rectangle " + (j + 1) + ":  " + rectang[j].getLength());
+			System.out.println("Width of rectangle " + (j + 1) + ":  " + rectang[j].getWidth());
+			System.out.println("Area of rectangle " + (j + 1) + ":  " + rectang[j].area());
 			System.out.println("------------------------------");
 		}
+		input.close();
 	}
 }
