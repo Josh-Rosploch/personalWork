301ef1a86a37c2eed8524c0b4cc8e859cf5fc11b
Josh Rosploch <josh.rosploch@gmail.com>
2018-12-17 14:56:05 -0600

    brain hurts

---
+++ src/DesigningClasses/MoreReTest.java
@@ -0,0 +1,12 @@
+package DesigningClasses;
+
+public class MoreReTest {
+	public static void main(String[] args) {
+		MoreRe bob = new MoreRe();
+		System.out.println(bob.sumTheNum(5));
+		System.out.println("-------------");
+		System.out.println(bob.sumTheOdd(10));
+		System.out.println("-------------");
+		System.out.println(bob.reverse(123456));
+	}
+}
