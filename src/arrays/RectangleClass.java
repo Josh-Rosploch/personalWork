b19df098c80ac5d7e2af1b3e5d877106cadc544b
Josh Rosploch <josh.rosploch@gmail.com>
2018-12-19 14:37:24 -0600

    the 3 methods

--- src/arrays/RectangleClass.java
+++ src/arrays/RectangleClass.java
@@ -37,4 +37,19 @@ public class RectangleClass {
 	public int area() {
 		return length * width;
 	}
+
+	public String toString() {
+		return ("Rectangle with length of " + length + " and width of " + width);
+	}
+
+	public boolean equals(Object otherObject) {
+		if (this.toString().equals(otherObject.toString())) {
+			return true;
+		}
+		return false;
+	}
+
+	public Object clone() {
+		return new RectangleClass(length, width);
+	}
 }
