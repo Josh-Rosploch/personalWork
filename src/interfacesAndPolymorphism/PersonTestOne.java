f41bed15d9f236e07191407c2c5cb28d1b238660
Josh Rosploch <josh.rosploch@gmail.com>
2019-01-07 09:59:08 -0600

    Stuff

--- src/interfacesAndPolymorphism/PersonTestOne.java
+++ src/interfacesAndPolymorphism/PersonTestOne.java
@@ -2,6 +2,22 @@ package interfacesAndPolymorphism;
 
 public class PersonTestOne {
 	public static void main(String[] args) {
-
+		Person bob = new Teacher();
+		System.out.println("Name " + bob.getName() + ". Age: " + bob.getAge());
+		bob = new Student();
+		System.out.println("Name " + bob.getName() + ". Age: " + bob.getAge());
+		// next
+		Teacher bil = new Teacher();
+		Student who = new Student();
+		bil.setName("Bill");
+		bil.payMe();
+		who.setName("Bobby");
+		who.outSick();
+		who.outSick();
+		who.outSick();
+		Person bile = who;
+		System.out.println("Name " + bile.getName() + ". Age: " + bile.getAge());
+		bile = who;
+		System.out.println("Name " + bile.getName() + ". Age: " + bile.getAge());
 	}
 }
